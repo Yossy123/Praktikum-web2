3c45c3ecdb2f7abcbdd3281101b24e6cefbb3e54
yossy123 <yossykusuma01@gmail.com>
2024-07-05 13:40:22 +0700

    akademik_kampus

---
+++ praktikum12/resources/views/kondisi.blade.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Kondisi</title>
+</head>
+<body>
+    @php
+    $nama : 'Yossy Indra Kusuma';
+    Echo "Hello" . $nama . '?';
+    @endphp
+</body>
+</html>
